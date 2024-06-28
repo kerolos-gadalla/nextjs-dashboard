345c8c6c1e926ffb8631585720da4381c7f7b5e6
Kerolos Gadalla <kerolos.sss@gmail.com>
2024-06-28 19:58:32 +0400

    fix

--- app/debug/page.tsx
+++ app/debug/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             {/* <AcmeLogo /> */}
-            {process.env.NEXT_PUBLIC_API_URL}
-            <pre>{JSON.stringify(moduleVar, null, 2)}</pre>
+            {/* {process.env.NEXT_PUBLIC_API_URL} */}
+            {/* <pre>{JSON.stringify(moduleVar, null, 2)}</pre> */}
           </div>
         </div>
       </div>
